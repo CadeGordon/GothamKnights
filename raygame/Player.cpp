54c6728aa90f166343b8aba2c22e5406e6ddcf91
Asiko6474 <Zacharycgrippo@gmail.com>
2022-01-03 12:02:11 -0600

    Changed the player to use red hood's picture

--- raygame/Player.cpp
+++ raygame/Player.cpp
@@ -12,7 +12,7 @@ void Player::start()
 	m_inputComponent = dynamic_cast<InputComponent*>(addComponent(new InputComponent()));
 	m_moveComponent = dynamic_cast<MoveComponent*> (addComponent(new MoveComponent()));
 	m_moveComponent->setMaxSpeed(10);
-	m_spriteComponent = dynamic_cast<SpriteComponent*> (addComponent(new SpriteComponent("Images/player.png")));
+	m_spriteComponent = dynamic_cast<SpriteComponent*> (addComponent(new SpriteComponent("Images/red hood.png")));
 	m_healthComponent = dynamic_cast<HealthComponent*> (addComponent(new HealthComponent()));
 	m_healthComponent->sethealth(10);
 
