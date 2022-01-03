e151b10e24a7f98b190eefb6c48745b97c7151fe
Cade Gordon <cadegordon093@gmail.com>
2022-01-03 16:53:42 -0600

    need to finish bullet class

--- raygame/Bullet.h
+++ raygame/Bullet.h
@@ -13,14 +13,20 @@ class Bullet :
 	public Actor
 {
 public:
+	Bullet();
 	Bullet(float x, float y, const char* name) : Actor(x, y, name) {}
+	~Bullet();
 
 	void start() override;
 	void update(float deltaTime) override;
 
 private:
-	InputComponent* m_inputComponent;
+	Actor* m_owner;
+	MathLibrary::Vector2 m_velocity;
 	MoveComponent* m_moveComponent;
 	SpriteComponent* m_spriteComponent;
+	MathLibrary::Vector2 m_startingPosition;
+	MathLibrary::Vector2 m_currentPosition;
+	
 };
 
